d55dd2c3220648c24de5975d88832aea55eb9aed
Tim A. Conner <timaconner1@gmail.com>
2017-11-28 17:53:21 -0600

    Added functionality to createPeopleDOM() to generate HTML of person and insert into dom.

--- wysiwyg.js
+++ wysiwyg.js
@@ -10,3 +10,71 @@ let people = [
         }
     }
 ];
+
+
+function createPerson(person){
+    let output = document.getElementById("output");
+    
+    let personDOM = document.createElement("person");
+    
+    //Create header dom
+    let headerDOM = document.createElement("header");
+
+    //Create name and insert
+    let nameDOM = document.createElement("h1");
+    var nameText = document.createTextNode(person.name);
+    nameDOM.appendChild(nameText);
+
+
+    //Create Title
+    let titleDOM = document.createElement("h2");
+    let titleText = document.createTextNode(person.title);
+    titleDOM.appendChild(titleText);
+
+    //insert name and title
+    headerDOM.appendChild(nameDOM);
+    headerDOM.appendChild(titleDOM);
+
+    //Create section Dom
+    let sectionDOM = document.createElement("section");
+
+    //Create bio
+    let bioDOM = document.createElement("p");
+    let bioText = document.createTextNode(person.bio);
+    bioDOM.appendChild(bioText);
+
+    //Create image
+    let imgDOM = document.createElement("img");
+    imgDOM.setAttribute("src", person.image);
+    imgDOM.setAttribute("alt", person.name);
+    
+    //Insert image
+    sectionDOM.appendChild(imgDOM);
+    sectionDOM.appendChild(bioDOM);
+
+    //Create footer dom
+    let footerDOM = document.createElement("footer");
+
+    //Create lifespan
+    let lifespanDOM = document.createElement("time");
+    let lifespanText = document.createTextNode(`${person.lifespan.birth} to  ${person.lifespan.death}`)
+    lifespanDOM.appendChild(lifespanText);
+
+    //insert lifespanDOM
+    footerDOM.appendChild(lifespanDOM);
+
+    //Insert header, section, footer, and person DOM
+    personDOM.appendChild(headerDOM);
+    personDOM.appendChild(sectionDOM);
+    personDOM.appendChild(footerDOM);
+    output.appendChild(personDOM);
+
+}
+
+function createPeopleDOM(){
+    people.forEach(function(element){
+        createPerson(element);
+    });
+}
+
+createPeopleDOM();
